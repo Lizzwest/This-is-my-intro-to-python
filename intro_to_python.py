f0fcb1124438967232ce02a80665a6448204a7ef
Lizz <lizwesterband@gmail.com>
2020-09-14 11:32:30 -0700

    some actually useful info

---
+++ intro_to_python.py
@@ -0,0 +1,15 @@
+ 
+#dictionary
+dog ={
+     "name": "Rocco",
+     "age" : "7",
+     "location" : "Los Angeles"
+     }
+print(dog)
+
+def print_this():
+    print("hello m,y name is...")
+
+# using the f keyword for string interpolation
+my_message = f"{dog['name']} lives in {dog['location']}"
+
